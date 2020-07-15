a12447d04f96e7403388df575ff7366bc22c5ba2
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-07-16 01:43:08 +0530

    Home Page Completed

--- Home/home.js
+++ Home/home.js
@@ -1,3 +1,9 @@
 window.addEventListener("DOMContentLoaded", function () {
   console.log("a");
+  var candidateLogin = document.getElementById("candidateLogin");
+  candidateLogin.addEventListener("click", onCandidateLogin);
 });
+
+var onCandidateLogin = function () {
+  location = "../Login/login.html";
+}
